e6a5ac9a65b3057886a337d9f1a748504f5b8f5b
guoqingshan <307185041@qq.com>
2017-06-03 11:32:07 +0800

    數據庫dao

--- app/control/home.py
+++ app/control/home.py
@@ -1,5 +1,8 @@
 import tornado
+from app.dao.bas_dao import BaseDao
 from app.control.basic import BasicControl
 class HomeControl(BasicControl):
     def get(self):
+        dao=BaseDao("posts")
+        print(dao.result('select * from posts'))
         self.render("posts.html")
---
+++ app/dao/bas_dao.py
@@ -0,0 +1,24 @@
+import sqlite3
+import sys
+import os
+from collections import OrderedDict
+class BaseDao(object):
+    def __init__ (self,name) :
+        self.con=sqlite3.connect(os.path.join(sys.path[0],"var","datas",name+".sdb"))
+        self.con.row_factory=self.__class__.sqlite_dict
+        self.con.text_factory =str
+    
+    @staticmethod
+    def sqlite_dict(cur,row):
+        return OrderedDict((v[0],row[i]) for i,v in enumerate(cur.description))
+
+    @staticmethod
+    def sqlite_rows(cur):
+        return [OrderedDict((v[0], row[i if i in row else v[0]]) for i, v in enumerate(cur.description)) for row in cur.fetchall()]
+
+    def result(self,*args,**kwargs):
+        cur=self.con.cursor()
+        cur.execute(*args,**kwargs)
+        ret=cur.fetchall()
+        cur.close()
+        return ret
